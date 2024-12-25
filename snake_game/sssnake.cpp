ffe84e62948a3284232eb46562734d26f59dc0c9
ROMA <romanchep2006@gmail.com>
2024-12-25 16:11:11 +0700

    all

--- snake_game/sssnake.cpp
+++ snake_game/sssnake.cpp
@@ -5,19 +5,28 @@
 using namespace std;
 
 bool gameOver;
-const int width = 15;
+const int width = 10;
 const int height = 10;
 int x, y, fruitX, fruitY, score;
 int tailX[100], tailY[100];
 int nTail;
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+enum eDirection
+{
+    STOP = 0,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
 eDirection dir;
 
-void SetColor(int color) {
+void SetColor(int color)
+{
     cout << "\033[" << color << "m";
 }
 
-void Setup() {
+void Setup()
+{
     gameOver = false;
     dir = STOP;
     x = width / 2;
@@ -27,78 +36,121 @@ void Setup() {
     score = 0;
 }
 
-void Draw() {
-    cout << "\033[H";
-
-    SetColor(33);
-    for (int i = 0; i < width; i++) cout << "# ";
-    cout << endl;
-
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            if (j == 0) {
-                SetColor(33);
-                cout << "# ";
-            }
-            if (i == y && j == x) {
-                SetColor(32);
-                cout << "O ";
-            }
-            else if (i == fruitY && j == fruitX) {
-                SetColor(31);
-                cout << "F ";
-            }
-            else {
-                bool print = false;
-                for (int k = 0; k < nTail; k++) {
-                    if (tailX[k] == j && tailY[k] == i) {
-                        SetColor(34);
-                        cout << "o ";
-                        print = true;
-                    }
-                }
-                if (!print) {
-                    SetColor(0);
-                    cout << "  ";
-                }
-            }
-            if (j == width - 1) {
-                SetColor(33);
-                cout << "# ";
-            }
+void DrawMessage(bool win)
+{
+    for (int i = 0; i < 2; i++)
+    {
+        if (win)
+        {
+            SetColor(32);
+            cout << "[ Y ] [ O ] [ U ] [   ] [ W ] [ I ] [ N ]\n";
         }
-        cout << endl;
+        else
+        {
+            SetColor(31);
+            cout << "[ G ] [ A ] [ M ] [ E ] [   ] [ O ] [ V ] [ E ] [ R ]\n";
+        }
     }
-
-    SetColor(33);
-    for (int i = 0; i < width; i++) cout << "# ";
-    cout << endl;
-
-    SetColor(36);
-    cout << "Score: " << score << endl;
-
     SetColor(0);
 }
 
-void Input() {
-    if (_kbhit()) {
-        switch (_getch()) {
-            case 'a': dir = LEFT; break;
-            case 'd': dir = RIGHT; break;
-            case 'w': dir = UP; break;
-            case 's': dir = DOWN; break;
-            case 'x': gameOver = true; break;
+void Draw()
+{
+    cout << "\033[H";
+    SetColor(31);
+    for (int i = 0; i < width; i++)
+        cout << "##";
+    cout << "##\n";
+    for (int i = 0; i < height; i++)
+    {
+        for (int line = 0; line < 2; line++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (j == 0)
+                {
+                    cout << "##";
+                }
+                if (i == y && j == x)
+                {
+                    SetColor(32);
+                    cout << "OO";
+                }
+                else if (i == fruitY && j == fruitX)
+                {
+                    SetColor(34);
+                    cout << "FF";
+                }
+                else
+                {
+                    bool print = false;
+                    for (int k = 0; k < nTail; k++)
+                    {
+                        if (tailX[k] == j && tailY[k] == i)
+                        {
+                            SetColor(32);
+                            cout << "oo";
+                            print = true;
+                        }
+                    }
+                    if (!print)
+                    {
+                        SetColor(0);
+                        cout << "  ";
+                    }
+                }
+                if (j == width - 1)
+                {
+                    SetColor(31);
+                    cout << "#as#";
+                }
+            }
+            cout << endl;
+        }
+    }
+    for (int i = 0; i < width; i++)
+        cout << "##";
+    cout << "##\n";
+
+    SetColor(36);
+    cout << "Score: " << score << endl;
+    SetColor(0);
+}
+
+void Input()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
         }
     }
 }
 
-void Logic() {
+void Logic()
+{
     int prevX = tailX[0];
     int prevY = tailY[0];
     int prev2X, prev2Y;
     tailX[0] = x;
     tailY[0] = y;
-    for (int i = 1; i < nTail; i++) {
+    for (int i = 1; i < nTail; i++)
+    {
         prev2X = tailX[i];
         prev2Y = tailY[i];
         tailX[i] = prevX;
@@ -106,36 +158,66 @@ void Logic() {
         prevX = prev2X;
         prevY = prev2Y;
     }
-    switch (dir) {
-        case LEFT: x--; break;
-        case RIGHT: x++; break;
-        case UP: y--; break;
-        case DOWN: y++; break;
-        default: break;
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
     }
 
-    if (x >= width) x = 0; else if (x < 0) x = width - 1;
-    if (y >= height) y = 0; else if (y < 0) y = height - 1;
-
-    for (int i = 0; i < nTail; i++) {
-        if (tailX[i] == x && tailY[i] == y) gameOver = true;
+    if (x < 0 || x >= width || y < 0 || y >= height)
+    {
+        gameOver = true;
+        DrawMessage(false);
+        return;
     }
 
-    if (x == fruitX && y == fruitY) {
+    for (int i = 0; i < nTail; i++)
+    {
+        if (tailX[i] == x && tailY[i] == y)
+        {
+            gameOver = true;
+            DrawMessage(false);
+            return;
+        }
+    }
+
+    if (x == fruitX && y == fruitY)
+    {
         score += 10;
         fruitX = rand() % width;
         fruitY = rand() % height;
         nTail++;
     }
+
+    if (nTail == width * height - 1)
+    {
+        gameOver = true;
+        DrawMessage(true);
+        return;
+    }
 }
 
-int main() {
+int main()
+{
     Setup();
-    while (!gameOver) {
+    while (!gameOver)
+    {
         Draw();
         Input();
         Logic();
-        Sleep(100);
+        Sleep(200);
     }
     return 0;
 }
--- tetris_game/tetris.cpp
+++ tetris_game/tetris.cpp
@@ -13,40 +13,32 @@ const int height = 20;
 vector<vector<int>> field(height, vector<int>(width, 0));
 
 const vector<vector<vector<int>>> figures = {
-    {
-        {1, 1},
-        {1, 1}
-    },
-    {
-        {1, 1, 1, 1}
-    },
-    {
-        {0, 1, 0},
-        {1, 1, 1}
-    },
-    {
-        {1, 0, 0},
-        {1, 1, 1}
-    },
-    {
-        {0, 0, 1},
-        {1, 1, 1}
-    }
-};
+    {{1, 1},
+     {1, 1}},
+    {{1, 1, 1, 1}},
+    {{0, 1, 0},
+     {1, 1, 1}},
+    {{1, 0, 0},
+     {1, 1, 1}},
+    {{0, 0, 1},
+     {1, 1, 1}}};
 
 vector<vector<int>> figure;
 int x = width / 2, y = 0;
 
-void spawnFigure() {
+void spawnFigure()
+{
     figure = figures[rand() % figures.size()];
     x = width / 2 - figure[0].size() / 2;
     y = 0;
 }
 
-bool checkCollision(int nx, int ny) {
+bool checkCollision(int nx, int ny)
+{
     for (int i = 0; i < figure.size(); i++)
         for (int j = 0; j < figure[i].size(); j++)
-            if (figure[i][j]) {
+            if (figure[i][j])
+            {
                 int fx = x + j + nx;
                 int fy = y + i + ny;
                 if (fx < 0 || fx >= width || fy >= height || field[fy][fx])
@@ -55,7 +47,8 @@ bool checkCollision(int nx, int ny) {
     return false;
 }
 
-void placeFigure() {
+void placeFigure()
+{
     for (int i = 0; i < figure.size(); i++)
         for (int j = 0; j < figure[i].size(); j++)
             if (figure[i][j])
@@ -63,22 +56,28 @@ void placeFigure() {
     spawnFigure();
 }
 
-void clearLines() {
-    for (int i = height - 1; i >= 0; i--) {
+void clearLines()
+{
+    for (int i = height - 1; i >= 0; i--)
+    {
         bool full = true;
         for (int j = 0; j < width; j++)
-            if (!field[i][j]) full = false;
-        if (full) {
+            if (!field[i][j])
+                full = false;
+        if (full)
+        {
             for (int k = i; k > 0; k--)
                 for (int j = 0; j < width; j++)
                     field[k][j] = field[k - 1][j];
-            for (int j = 0; j < width; j++) field[0][j] = 0;
+            for (int j = 0; j < width; j++)
+                field[0][j] = 0;
             i++;
         }
     }
 }
 
-void rotateFigure() {
+void rotateFigure()
+{
     vector<vector<int>> rotated(figure[0].size(), vector<int>(figure.size()));
 
     for (int i = 0; i < figure.size(); i++)
@@ -87,7 +86,8 @@ void rotateFigure() {
 
     figure = rotated;
 
-    if (checkCollision(0, 0)) {
+    if (checkCollision(0, 0))
+    {
         for (int i = 0; i < figure.size(); i++)
             for (int j = 0; j < figure[i].size(); j++)
                 rotated[j][figure.size() - 1 - i] = 0;
@@ -96,10 +96,14 @@ void rotateFigure() {
     }
 }
 
-void draw() {
-    for (int i = 0; i < 30; i++) cout << "\n";
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+void draw()
+{
+    for (int i = 0; i < 30; i++)
+        cout << "\n";
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             bool isFigure = false;
             for (int fi = 0; fi < figure.size(); fi++)
                 for (int fj = 0; fj < figure[fi].size(); fj++)
@@ -110,44 +114,56 @@ void draw() {
         cout << "||" << endl;
     }
 
-    for (int j = 0; j < width + 7; j++) cout << "=";
+    for (int j = 0; j < width + 7; j++)
+        cout << "=";
     cout << "=====" << endl;
-    cout << "A - влево | D - вправо | S - ускорить | Q - выход | W - вращать\n";
+    cout << "A - vlevo | D - vpravo | S - speedup | Q - vihod | W - vrash'at'\n";
 }
 
-
-void input() {
-    if (_kbhit()) {
+void input()
+{
+    if (_kbhit())
+    {
         char key = _getch();
-        if (key == 'a' && !checkCollision(-1, 0)) x--;
-        if (key == 'd' && !checkCollision(1, 0)) x++;
-        if (key == 's' && !checkCollision(0, 1)) y++;
-        if (key == 'w') rotateFigure();
-        if (key == 'q') exit(0);
+        if (key == 'a' && !checkCollision(-1, 0))
+            x--;
+        if (key == 'd' && !checkCollision(1, 0))
+            x++;
+        if (key == 's' && !checkCollision(0, 1))
+            y++;
+        if (key == 'w')
+            rotateFigure();
+        if (key == 'q')
+            exit(0);
     }
 }
-void logic() {
+void logic()
+{
     if (!checkCollision(0, 1))
         y++;
-    else {
+    else
+    {
         placeFigure();
         clearLines();
-        if (checkCollision(0, 0)) {
+        if (checkCollision(0, 0))
+        {
             cout << "Game Over!" << endl;
             exit(0);
         }
     }
 }
 
-int main() {
+int main()
+{
     srand(time(0));
     spawnFigure();
 
-    while (true) {
+    while (true)
+    {
         draw();
         input();
         logic();
-        Sleep(200);
+        Sleep(100);
     }
     return 0;
 }
